149450d85b4d1255b5391635cafee09589fc117e
artempetich <artempetich@yandex.ru>
2024-03-19 21:15:16 +0500

    HW

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ToggleThemeRedux from "./components/toggleThemeRedux";
+
+function App() {
+  return (
+    <div>
+      <ToggleThemeRedux />
+    </div>
+  );
+}
+
+export default App;
